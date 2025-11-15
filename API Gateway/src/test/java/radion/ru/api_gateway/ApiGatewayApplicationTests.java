0139ac0823ace0fe1d240fd1793d147c26147df8
Radion <beslaneev.radion@yandex.ru>
2025-11-16 02:05:50 +0300

    Init start configuration project

---
+++ API Gateway/src/test/java/radion/ru/api_gateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package radion.ru.api_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
